a1ed7f6fd293818d2468a4e863a5c9727e2b85cc
filippov nikita <filippovnr73@gmail.com>
2023-09-08 18:59:34 +0300

    hotfix dockstring'ов

--- str_func.py
+++ str_func.py
@@ -1,6 +1,11 @@
 def upper(word=''):
-    """тут должно быть описание"""
+    """
+    тут должно быть описание
+    """
     return word.upper()
 
 def to_tittle(word=''):
+    """
+        тут ТОЖЕ должно быть описание
+    """
     return word.title()
